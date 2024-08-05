1023c06bc408a0d67b80c0a869e343e38bed496a
DavidZomber935 <davidbra12345@gmail.com>
2024-08-05 07:58:52 -0500

    primer commit

---
+++ src/main/java/com/proyectofinal/SpringProyectoISI/SpringProyectoIsiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.SpringProyectoISI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyectoIsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyectoIsiApplication.class, args);
+	}
+
+}
